5800d6d2f72ff05bdef8c0760bbe44e0c0b5de9b
litaamelia <litaamelia126@gmail.com>
2023-02-28 17:26:31 +0700

    declaration expression & arrow function

---
+++ js/arrow.js
@@ -0,0 +1,26 @@
+//Function Declaration
+// function kuadrat(a) {
+//     return a*a;
+// }
+
+//Function Expression
+// const kuadrat = function(a) {
+//     return a*a;
+// }
+
+//Arrow Function
+// const kuadrat = (a) => {
+//     return a*a;
+// }
+// console.log(kuadrat(3));
+
+// const kuadrat = a => a*a;
+// console.log(kuadrat(3));
+
+const kuadrat = (a) => {
+    a = a+1;
+    return a*a;
+}
+console.log(kuadrat(3));
+
+/**Menggunakan kurung kurawal jika statementnya lebih dari satu */
